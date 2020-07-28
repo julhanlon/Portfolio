9df8e4c698a92caa1c0253a3930da7e8d8afc6af
julhanlon <jhanlon289@gmail.com>
2020-07-27 21:53:37 -0700

    fixes

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
-$(document).ready(function () {
-  console.log("hello");
+// $(document).ready(function () {
+//   console.log("hello");
 
-  $("#portfolio").hide();
-  $("#footer").hide();
-  $(".navbar").hide();
+//   $("#portfolio").hide();
+//   $(".navbar").hide();
+//   $("#landing-img").hide();
 
-  $("#opening").fadeOut(3500);
-  hideOpening();
+//   $("#opening").fadeOut(3500);
+//   hideOpening();
 
-  var timer = "";
-  function hideOpening() {
-    timer = setTimeout(function () {
-      $("#opening").hide();
-      $("#portfolio").fadeIn(2000);
-      $("#footer").show();
-      $(".navbar").fadeIn(2000);
-    }, 3000);
-  }
-});
+//   var timer = "";
+//   function hideOpening() {
+//     timer = setTimeout(function () {
+//       $("#opening").hide();
+//       $("#landing-img").show();
+//       $("#portfolio").fadeIn(2000);
+//       $(".navbar").fadeIn(2000);
+//     }, 3000);
+//   }
+// });
